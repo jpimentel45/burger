0b671d698e586d00a92a4ec72d6eed49c8d3953e
Juanito <Juanito@juans-mbp.lan>
2019-06-25 15:54:30 -0700

    half files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', false);
