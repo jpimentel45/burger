a0a8fbd4ee227dd668d29bb3a3c418047f47f15d
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-06-28 14:45:12 -0700

    changing password

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "nauj1994",
   database: "burgers_db"
 });
 
@@ -16,5 +16,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export to ORM 
+// Export to ORM
 module.exports = connection;
